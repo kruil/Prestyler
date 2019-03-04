25f537b1e14c92fbdceae23c62aa0282a639574b
Ilia Krupko <kruil@Administrators-MacBook-Pro-3.local>
2019-03-04 15:50:57 +0300

    Example was reviewed.

--- Example/Prestyler/ViewController.swift
+++ Example/Prestyler/ViewController.swift
@@ -17,29 +17,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var label4: UILabel!
     @IBOutlet weak var label5: UILabel!
     @IBOutlet weak var label6: UILabel!
-
+    @IBOutlet weak var label7: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // use predefined rules:
+        label1.attributedText = "<b>Hello world!<b> I am a cool pod for <underline>Swift<underline>.".prestyled()
 
-        Prestyler.defineRule("<red>", UIColor.red, Prestyle.bold,  22)
-        label1.attributedText = "<red>Hello world!<red> I <strike>am<strike> a <b>cool<b> pod for Swift.".prestyled()
+        // or define your owns:
+        Prestyler.defineRule("$", 28, UIColor.orange, Prestyle.italic)
+        label2.attributedText = "Style $text$ in an easy way.".prestyled()
 
-        Prestyler.defineRule("$", UIFont.italicSystemFont(ofSize: 33), UIColor.orange)
-        label2.attributedText = "Style $all text$ in an <i>easy way<i>.".prestyled()
+        Prestyler.defineRule("<yellowOnBlue>", UIColor.blue, Prestyle.bold)
+        label3.attributedText = "<yellowOnBlue> Quick and free<yellowOnBlue> for use.".prestyled()
 
-        Prestyler.defineRule("<yellowOnBlue>", UIColor.blue, Precolor(.yellow).forBackgound(), Prestyle.bold)
-        label3.attributedText = "<yellowOnBlue> Quick <yellowOnBlue> and <underline>free<underline> for use.".prestyled()
-
+        // Use Precolor class to set foreground and backgound colors
         Prestyler.defineRule("<coolgreen>", Precolor("#550"), Precolor("#a7f442").forBackgound())
-        Prestyler.defineRule("*", "#f0f")
-        label4.attributedText = "Custom <coolgreen>colors<coolgreen> and *different* rules.".prestyled()
+        label4.attributedText = "Custom <coolgreen>colors<coolgreen> and different rules.".prestyled()
 
         Prestyler.defineRule("<mixed>", Precolor(.red).random(50), 33)
-        label5.attributedText = "<mixed>Create cool effects<mixed> ".prestyled()
+        Prestyler.defineRule("<random>", "#fff", Precolor().random().forBackgound(), 36)
+        label5.attributedText = "<mixed>Create cool<mixed> <random>effects<random>".prestyled()
 
-        Prestyler.defineRule("<random>", "#fff", Precolor().random().forBackgound(), 48)
-        label6.attributedText = "Prestyler".prestyledBy(rule: "<random>")
-        //§label6.attributedText = "Prestyler".prestyledBy(styles: UIColor.green)
+        // You can easy style any text without tags by rule name or style list
+        label6.attributedText = "Short and simple syntax.".prestyledBy(rule: "<b>")
+        label7.attributedText = "You will like me! ❤️".prestyledBy(styles: "#e38", Prestyle.bold)
     }
 }
 
