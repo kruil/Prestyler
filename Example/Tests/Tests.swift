d7b912451f3ff13badf6c3d0e75d05958ff63569
Ilia Krupko <kruil@182.1.192.168.lan.quantum.ru>
2019-03-19 15:22:46 +0300

    Tests added.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,28 +1,44 @@
 import XCTest
-import Prestyler
+@testable import Prestyler
+
 
 class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func testFindRules() {
+        var text = "That is <b>a<b> text."
+        var rules = Prestyler.findTextRules(&text)
+        XCTAssert(rules[0].positions == [8,9])
+        XCTAssert(text == "That is a text.")
+        Prestyler.defineRule("*", UIColor.red)
+        Prestyler.defineRule("^", UIColor.blue)
+        text = "^text^ *743* ^text^ ^text^ *716979*"
+        rules = Prestyler.findTextRules(&text)
+        XCTAssert(text == "text 743 text text 716979")
+        XCTAssert(rules.count == 2)
+        XCTAssert(rules[0].positions == [5, 8, 19, 25])
+        XCTAssert(rules[1].positions == [0, 4, 9, 13, 14, 18])
+    }
+
+    func testPrefilter() {
+        let prefilteredNumbers = "text 743 text text 716979".prefilter(type: .numbers, by: "*")
+        XCTAssert(prefilteredNumbers == "text *743* text text *716979*")
+        let prefilteredTWord = "text 743 text text 716979".prefilter(text: "text", by: "^")
+        XCTAssert(prefilteredTWord == "^text^ 743 ^text^ ^text^ 716979")
+        let prefilteredBoth = "text 743 text text 716979"
+            .prefilter(type: .numbers, by: "*")
+            .prefilter(text: "text", by: "^")
+        let prefilteredBothInvert = "text 743 text text 716979"
+            .prefilter(text: "text", by: "^")
+            .prefilter(type: .numbers, by: "*")
+        XCTAssert(prefilteredBoth == "^text^ *743* ^text^ ^text^ *716979*")
+        XCTAssert(prefilteredBoth == prefilteredBothInvert)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testCorrectPositions() {
+        var positions = [0,5,11,16,18,23]
+        var rules = [TextRule(styles: [], positions: [7,10,25,31])]
+        Prestyler.correctPositions(&positions, 1, &rules)
+        XCTAssert(positions == [0,4,9,13,14,18])
+        XCTAssert(rules[0].positions == [5,8,19,25])
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
