50b3118dbd20e21d6ea4fa734e2ee71211fd872b
Ilia Krupko <kruil@182.1.192.168.lan.quantum.ru>
2019-03-19 15:22:36 +0300

    Example app uses prefilter().

--- Example/Prestyler/ViewController.swift
+++ Example/Prestyler/ViewController.swift
@@ -18,11 +18,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var label5: UILabel!
     @IBOutlet weak var label6: UILabel!
     @IBOutlet weak var label7: UILabel!
-    
+    @IBOutlet weak var label8: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // use predefined rules:
-        label1.attributedText = "<b>Hello world!<b> I'm a cool <underline>cocoapod<underline>.".prestyled()
+        label1.attributedText = "<b>Hello world!<b> I'm <underline>Prestyler<underline>.".prestyled()
 
         // or define your owns:
         Prestyler.defineRule("$", 32, UIColor.orange, Prestyle.italic)
@@ -35,13 +37,22 @@ class ViewController: UIViewController {
         Prestyler.defineRule("<coolgreen>", Precolor("#550"), Precolor("#a7f442").forBackgound())
         label4.attributedText = "Custom <coolgreen>colors<coolgreen> and different rules.".prestyled()
 
-        Prestyler.defineRule("<mixed>", Precolor(.red).random(50), 44)
-        Prestyler.defineRule("<random>", "#fff", Precolor().random().forBackgound(), 44)
+        Prestyler.defineRule("<mixed>", Precolor(.red).random(50), 42)
+        Prestyler.defineRule("<random>", "#fff", Precolor().random().forBackgound(), 42)
         label5.attributedText = "<mixed>Create cool<mixed> <random>effects<random>".prestyled()
 
+        // Filter what you need and style it
+        Prestyler.defineRule("*", UIColor.red)
+        Prestyler.defineRule("^", UIColor.blue)
+        let text = "Highlight text or numbers like 23 or 865 in your text using Prefilter. " +
+                   "It parses your text, embrace numbers 73 and 1234, and you are ready to go."
+        let prefilteredText = text.prefilter(type: .numbers, by: "*").prefilter(text: "text", by: "^")
+        label6.attributedText = prefilteredText.prestyled()
+
         // You can easy style any text without tags by rule name or style list
-        label6.attributedText = "Short and simple syntax.".prestyledBy(rule: "<b>")
-        label7.attributedText = "You will like me! ❤️".prestyledBy(styles: "#e38", Prestyle.bold)
+        label7.attributedText = "Short and simple syntax.".prestyledBy(rule: "<b>")
+        label8.attributedText = "You will like it! ❤️ ".prestyledBy(styles: "#e38", Prestyle.bold)
+
     }
 }
 
